36f0cbe7ab6d9eb022d65c32447fd34db8ae1d94
Brandon Falk <bfalk@gamozolabs.com>
2020-09-27 06:25:53 -0700

    Can route to UC wall

--- src/main.rs
+++ src/main.rs
@@ -15,22 +15,49 @@ use sdl2::event::{Event, WindowEvent};
 use sdl2::video::SwapInterval;
 use sdl2::keyboard::Keycode;
 
+use parse_ealogpos::Positions;
+
 // Vertex shader
 static VS_SRC: &'static str = "
 #version 150
 in vec3 position;
 uniform mat4 transform_matrix;
+out vec3 vs_pos;
 
 out VS_OUT {
     vec3 orig_position;
 } vs_out;
 
 void main() {
+    vs_pos = position;
     vs_out.orig_position = position;
     gl_Position = transform_matrix * vec4(position.x, position.y, position.z, 1.0);
 }";
 
-// Fragment shader
+// Line vertex shader
+static LINE_VS_SRC: &'static str = "
+#version 150
+in vec4 position;
+uniform mat4 transform_matrix;
+out vec4 vs_pos;
+
+void main() {
+    vs_pos = position;
+    gl_Position = transform_matrix * vec4(position.x, position.y, position.z, 1.0);
+}";
+
+// Line fragment shader
+static LINE_FS_SRC: &'static str = "
+#version 150
+out vec4 out_color;
+in vec4 vs_pos;
+
+void main() {
+    out_color = vec4(0.0, vs_pos.w, 0.0, 1.0);
+}
+";
+
+// Triangle fragment shader
 static FS_SRC: &'static str = "
 #version 150
 
@@ -80,11 +107,10 @@ void main() {
     //   0 degrees = Flat surface, eg, flat terrain
     //  90 degrees = Straight vertical
     // 180 degrees = Flat surface, but upside-down, like looking at a ceiling
-    float slope = degrees(acos(abs(normal.y)));
+    float slope = degrees(acos(max(min(1.0, abs(normal.y)), 0.0)));
 
-    // This is a linear gradient from:
-    //  0 degree slope, flat surface, color = 0.8, 0.8. 0.8
-    // 60 degree slope, flat surface, color = 0.2, 0.2. 0.2
+    // Color is 0.1 0.1 0.1 for steepest slope
+    // Color is 0.9 0.9 0.9 for flattest slope
     vec4 color = vec4(0.1, 0.1, 0.1, 1.0) + (90 - slope) / 112.5;
     
     // Color unclimbable slopes a bit more blue
@@ -147,12 +173,14 @@ fn compile_shader(src: &str, ty: GLenum) -> GLuint {
     shader
 }
 
-fn link_program(vs: GLuint, fs: GLuint, gs: GLuint) -> GLuint {
+fn link_program(vs: GLuint, fs: GLuint, gs: Option<GLuint>) -> GLuint {
     unsafe {
         let program = gl::CreateProgram();
         gl::AttachShader(program, vs);
         gl::AttachShader(program, fs);
-        gl::AttachShader(program, gs);
+        if let Some(gs) = gs {
+            gl::AttachShader(program, gs);
+        }
         gl::LinkProgram(program);
         // Get the link status
         let mut status = gl::FALSE as GLint;
@@ -250,6 +278,116 @@ pub fn main() {
         return;
     }
     
+    let mut head_pos: Point3<f32> = Point3::new(0., 1000., 0.);
+
+    print!("Loading LUA data...\n");
+
+    let mut path_data: Vec<(f32, f32, f32, f32)> = Vec::new();
+            
+    // Tgt triangle
+    // 186.73837, 95.57352, 1843.6765
+    // 176.75186, 96.93022, 1838.2499
+    //let land_target_x = 1838.2499;
+    //let land_target_y = 176.75186;
+    let land_target_x = (1843.6765 + 1838.2499) / 2.;
+    let land_target_y = (186.73837 + 176.75186) / 2.;
+
+    let positions = Positions::from_lua("gg_uc_zep.lua")
+        .expect("Failed to load LUA path");
+    let mut closest = std::f64::MAX;
+    let mut idx = Cell::new(1);
+    positions.positions.windows(2)
+        .filter(|poss| {
+            idx.set(idx.get() + 1);
+            let y_delta = poss[1].y - poss[0].y;
+            let x_delta = poss[1].x - poss[0].x;
+            let delta = ((y_delta * y_delta) + (x_delta * x_delta)).sqrt();
+            poss[0].map_id == 0 && poss[0].map_id == poss[1].map_id &&
+                delta < 10.
+        })
+        .for_each(|poss| {
+            let x_delta = land_target_x - poss[0].x;
+            let y_delta = land_target_y - poss[0].y;
+            let delta_target =
+                ((y_delta * y_delta) + (x_delta * x_delta)).sqrt();
+
+            let delta_pct = delta_target / 121.75096014;
+
+            let color = 1.0 - ((delta_pct - 1.0) * 500.).min(1.0);
+            let color = color as f32;
+
+
+            if delta_target < closest {
+                closest = delta_target;
+                //print!("{}\n", closest);
+            }
+
+            if delta_target == 121.75096014778174 {
+                print!("WOO CLOSEST\n");
+
+                let delta_x = (poss[0].x - land_target_x).abs();
+                let delta_y = (poss[0].y - land_target_y).abs();
+
+                let jump_angle = (delta_y / delta_x).atan().to_degrees();
+                print!("X {} Y {} jump from\n", poss[0].x, poss[0].y);
+                print!("{} absolute angle\n", 180. - jump_angle);
+                print!("{} stationary boat angle\n",
+                       (180. - (poss[0].angle - 82.691023903837)) - jump_angle);
+            
+                head_pos.x = poss[0].y as f32;
+                head_pos.z = poss[0].x as f32;
+
+                path_data.push((poss[0].y as f32, 100.,
+                                poss[0].x as f32, 0.5));
+                path_data.push((poss[0].y as f32, 100.,
+                                poss[0].x as f32 - 1000., 0.5));
+                
+                path_data.push((poss[0].y as f32, 100.,
+                                poss[0].x as f32, 0.5));
+                path_data.push((land_target_y as f32, 100.,
+                                land_target_x as f32, 0.5));
+            }
+
+            path_data.push((poss[0].y as f32, 100.,
+                            poss[0].x as f32, color));
+            path_data.push((poss[1].y as f32, 100.,
+                            poss[1].x as f32, color));
+        });
+        
+    // A decrease in our X (WoW's Y) is west
+    // An increase in our Z (WoW's X) is north
+
+    let positions = Positions::from_lua("jumpatt2_success.lua")
+        .expect("Failed to load LUA path");
+    positions.positions.windows(2)
+        .filter(|poss| {
+            idx.set(idx.get() + 1);
+            let y_delta = poss[1].y - poss[0].y;
+            let x_delta = poss[1].x - poss[0].x;
+            let delta = ((y_delta * y_delta) + (x_delta * x_delta)).sqrt();
+            poss[0].map_id == 0 && poss[0].map_id == poss[1].map_id &&
+                delta < 10.
+        })
+        .for_each(|poss| {
+            path_data.push((poss[0].y as f32, 100., poss[0].x as f32, 0.1));
+            path_data.push((poss[1].y as f32, 100., poss[1].x as f32, 0.1));
+        });
+    let positions = Positions::from_lua("jumpatt.lua")
+        .expect("Failed to load LUA path");
+    positions.positions.windows(2)
+        .filter(|poss| {
+            idx.set(idx.get() + 1);
+            let y_delta = poss[1].y - poss[0].y;
+            let x_delta = poss[1].x - poss[0].x;
+            let delta = ((y_delta * y_delta) + (x_delta * x_delta)).sqrt();
+            poss[0].map_id == 0 && poss[0].map_id == poss[1].map_id &&
+                delta < 10.
+        })
+        .for_each(|poss| {
+            path_data.push((poss[0].y as f32, 100., poss[0].x as f32, 0.3));
+            path_data.push((poss[1].y as f32, 100., poss[1].x as f32, 0.3));
+        });
+    
     print!("Loading falkvbo data...\n");
 
     // Get the vertex data and indicies for the data in our object file
@@ -298,10 +436,18 @@ pub fn main() {
     let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
     let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
     let gs = compile_shader(GS_SRC, gl::GEOMETRY_SHADER);
-    let program = link_program(vs, fs, gs);
+    let program = link_program(vs, fs, Some(gs));
+    
+    // Create GLSL shaders
+    let line_vs = compile_shader(LINE_VS_SRC, gl::VERTEX_SHADER);
+    let line_fs = compile_shader(LINE_FS_SRC, gl::FRAGMENT_SHADER);
+    let line_program = link_program(line_vs, line_fs, None);
 
     let mut vao = 0;
+    let mut path_vao = 0;
     let mut vbo = 0;
+    let mut ele_buffer = 0;
+    let mut path_buffer = 0;
 
     unsafe {
         // Create Vertex Array Object
@@ -322,8 +468,6 @@ pub fn main() {
         gl::UseProgram(program);
         gl::BindFragDataLocation(program, 0,
                                  CString::new("out_color").unwrap().as_ptr());
-
-        // Specify the layout of the vertex data
         let pos_attr = gl::GetAttribLocation(program,
                                              CString::new("position")
                                              .unwrap().as_ptr());
@@ -337,12 +481,40 @@ pub fn main() {
             std::ptr::null(),
         );
 
-        let mut ele_buffer = 0;
         gl::GenBuffers(1, &mut ele_buffer);
         gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ele_buffer);
         gl::BufferData(gl::ELEMENT_ARRAY_BUFFER,
             core::mem::size_of_val(&triangles[..]) as isize,
             triangles.as_ptr() as *const _, gl::STATIC_DRAW);
+        
+        // Set up the path VAO
+        gl::GenVertexArrays(1, &mut path_vao);
+        gl::BindVertexArray(path_vao);
+       
+        // Create a buffer for vertex data for the path
+        gl::GenBuffers(1, &mut path_buffer);
+        gl::BindBuffer(gl::ARRAY_BUFFER, path_buffer);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            core::mem::size_of_val(&path_data[..]) as isize,
+            path_data.as_ptr() as *const _,
+            gl::STATIC_DRAW,
+        );
+        gl::UseProgram(line_program);
+        gl::BindFragDataLocation(line_program, 0,
+                                 CString::new("out_color").unwrap().as_ptr());
+        let pos_attr = gl::GetAttribLocation(line_program,
+                                             CString::new("position")
+                                             .unwrap().as_ptr());
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(
+            pos_attr as GLuint,
+            4,
+            gl::FLOAT,
+            gl::FALSE as GLboolean,
+            0,
+            std::ptr::null(),
+        );
 
         gl::Enable(gl::CULL_FACE);
         gl::Enable(gl::DEPTH_TEST);
@@ -354,14 +526,16 @@ pub fn main() {
     // Name of the camera uniform
     let transform_matrix_name = CString::new("transform_matrix").unwrap();
 
-    let mut head_pos: Point3<f32> = Point3::new(0., 1000., 0.);
     let mut head_horiz_angle: f32 = 0.;
-    let mut head_vert_angle:  f32 = (-89f32).to_radians();
+    let mut head_vert_angle:  f32 = (-89.9999999f32).to_radians();
     let mut move_speed:       f32 = 10.;
 
     // Find the transform matrix location
     let transform_matrix_loc = unsafe {
         gl::GetUniformLocation(program, transform_matrix_name.as_ptr())
+    };
+    let transform_matrix_loc_line = unsafe {
+        gl::GetUniformLocation(line_program, transform_matrix_name.as_ptr())
     };
 
     let update_transforms = |origin: &mut Point3<f32>, horiz_angle: f32, vert_angle: f32, movement_front: f32, movement_strafe: f32| {
@@ -379,7 +553,7 @@ pub fn main() {
 
         *origin += direction * movement_front;
         *origin += direction_strafe * movement_strafe;
-
+        
         let proj_matrix: Matrix4<f32> =
             perspective(Deg(45.),
                 win_width.get() as f32 / win_height.get() as f32,
@@ -387,10 +561,17 @@ pub fn main() {
         let view_matrix: Matrix4<f32> =
             Matrix4::look_at(*origin, *origin + direction, Vector3::new(0., 1., 0.));
         let transform_matrix = proj_matrix * view_matrix;
+        
+        print!("Character at {:?}\n", origin);
 
         // Update the uniform
         unsafe {
+            gl::UseProgram(program);
             gl::UniformMatrix4fv(transform_matrix_loc, 1,
+                gl::FALSE as GLboolean,
+                AsRef::<[f32; 16]>::as_ref(&transform_matrix).as_ptr());
+            gl::UseProgram(line_program);
+            gl::UniformMatrix4fv(transform_matrix_loc_line, 1,
                 gl::FALSE as GLboolean,
                 AsRef::<[f32; 16]>::as_ref(&transform_matrix).as_ptr());
         }
@@ -417,8 +598,15 @@ pub fn main() {
             unsafe {
                 gl::ClearColor(0.0, 0.0, 0.0, 1.0);
                 gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+      
+                gl::UseProgram(program);
+                gl::BindVertexArray(vao);
                 gl::DrawElements(gl::TRIANGLES, triangles.len() as i32 * 3,
                     gl::UNSIGNED_INT, core::ptr::null_mut());
+                
+                gl::UseProgram(line_program);
+                gl::BindVertexArray(path_vao);
+                gl::DrawArrays(gl::LINES, 0, path_data.len() as i32);
             }
 
             // Set that the frame has not changed
@@ -506,8 +694,8 @@ pub fn main() {
                         let ydel = yrel as f32 / 400.;
                         head_horiz_angle = head_horiz_angle - xdel;
                         head_vert_angle  = (head_vert_angle  - ydel)
-                            .min(std::f32::consts::PI / 2. - 0.01)
-                            .max(-std::f32::consts::PI / 2. + 0.01);
+                            .min(std::f32::consts::PI / 2. - 0.0001)
+                            .max(-std::f32::consts::PI / 2. + 0.0001);
                         update_transforms(&mut head_pos,
                             head_horiz_angle, head_vert_angle, 0., 0.);
                         frame_changed = true;
